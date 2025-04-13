45f9008f570fc5285c6b48b29a9bfab52f3275c7
dunamix <techwithdunamix@gmail.com>
2025-04-13 14:22:36 +0100

    Update metadata in layout.tsx for Duna Draw application

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Duna Draw ',
+  description: 'created by techwithdunamix',
+  generator: 'https://github.com/techwithdunamix',
 }
 
 export default function RootLayout({
